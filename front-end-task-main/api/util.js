ca417a8e5b77ba6d5d4cb61e038b873a84d91982
Brendan Ugo-Emeribe <Brendan@circleleasing.co.uk>
2025-04-03 14:04:08 +0100

    Remove duplicate folder

--- front-end-task-main/api/util.js
+++
@@ -1,9 +0,0 @@
-import { randomBytes } from "crypto";
-
-export function uuid() {
-    return randomBytes(16).toString("hex");
-}
-
-export function round(number, decimals) {
-    return Number(`${Math.round(`${number}e+${decimals}`)}e-${decimals}`);
-}
